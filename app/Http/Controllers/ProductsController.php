56670e53cdeb66ec3eae48cad5775b9c3f0f319d
khatabwedaa <khatabwedaa@gmail.com>
2023-03-24 20:20:19 +0200

    WIP

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -37,6 +37,7 @@ class ProductsController extends Controller
             'name' => $attributes['name'],
             'cost' => $attributes['cost'],
         ]);
+
         $units = collect($attributes['units'])->map(function ($unit) {
             return [
                 'name' => $unit['name'],
@@ -51,7 +52,7 @@ class ProductsController extends Controller
 
     public function import()
     {
-        Excel::import(new ProductImport, request()->file('file'));
+        Excel::import(new ProductImport(), request()->file('file'));
 
         return back()->with('flash', ['message' => 'Imported successfully']);
     }
--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -9,8 +9,8 @@ const props = defineProps({
 
 const classes = computed(() => {
     return props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-emerald-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-emerald-700 transition'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition';
+        ? 'flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 bg-gray-100 dark:bg-gray-800 dark:text-gray-200 text-gray-700'
+        : 'flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700';
 });
 </script>
 
