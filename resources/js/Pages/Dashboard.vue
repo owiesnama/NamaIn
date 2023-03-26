1af23c483af0379969b06135b2efe8a39a26a31c
Owiesnama <owiesnama@gmail.com>
2023-03-27 00:00:52 +0200

    wip

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
+    import AppLayout from "@/Layouts/AppLayout.vue";
 </script>
 
 <template>
@@ -10,84 +10,205 @@ import AppLayout from '@/Layouts/AppLayout.vue';
             </h2>
         </template>
 
-        <div class="w-full ">
-            <h3 class="text-base font-semibold leading-6 text-gray-800">Last 30 days</h3>
+        <div class="w-full">
+            <h3 class="text-base font-semibold leading-6 text-gray-800">
+                Last 30 days
+            </h3>
 
-            <dl class="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-3">
-                <div class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6">
+            <dl
+                class="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-3"
+            >
+                <div
+                    class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6"
+                >
                     <dt>
                         <div class="absolute p-3 rounded-md bg-emerald-500">
-                        <svg class="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                        </svg>
+                            <svg
+                                class="w-6 h-6 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                                />
+                            </svg>
                         </div>
-                        <p class="ml-16 text-sm font-medium text-gray-500 truncate">Total Subscribers</p>
+                        <p
+                            class="ml-16 text-sm font-medium text-gray-500 truncate"
+                        >
+                            Total Sales
+                        </p>
                     </dt>
                     <dd class="flex items-baseline pb-6 ml-16 sm:pb-7">
-                        <p class="text-2xl font-semibold text-gray-900">71,897</p>
-                        <p class="flex items-baseline ml-2 text-sm font-semibold text-green-600">
-                        <svg class="self-center flex-shrink-0 w-5 h-5 text-green-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z" clip-rule="evenodd" />
-                        </svg>
-                        <span class="sr-only"> Increased by </span>
-                        122
+                        <p class="text-2xl font-semibold text-gray-900">
+                            71,897
                         </p>
-                        <div class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6">
-                        <div class="text-sm">
-                            <a href="#" class="font-medium text-emerald-600 hover:text-emerald-500"> View all<span class="sr-only"> Total Subscribers stats</span></a>
-                        </div>
+                        <p
+                            class="flex items-baseline ml-2 text-sm font-semibold text-green-600"
+                        >
+                            <svg
+                                class="self-center flex-shrink-0 w-5 h-5 text-green-500"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span class="sr-only"> Increased by </span>
+                            122
+                        </p>
+                        <div
+                            class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6"
+                        >
+                            <div class="text-sm">
+                                <a
+                                    href="#"
+                                    class="font-medium text-emerald-600 hover:text-emerald-500"
+                                >
+                                    View all<span class="sr-only">
+                                        Total Subscribers stats</span
+                                    ></a
+                                >
+                            </div>
                         </div>
                     </dd>
                 </div>
 
-                <div class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6">
+                <div
+                    class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6"
+                >
                     <dt>
                         <div class="absolute p-3 rounded-md bg-emerald-500">
-                        <svg class="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
-                        </svg>
+                            <svg
+                                class="w-6 h-6 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+                                />
+                            </svg>
                         </div>
-                        <p class="ml-16 text-sm font-medium text-gray-500 truncate">Avg. Open Rate</p>
+                        <p
+                            class="ml-16 text-sm font-medium text-gray-500 truncate"
+                        >
+                            Total Purchases
+                        </p>
                     </dt>
                     <dd class="flex items-baseline pb-6 ml-16 sm:pb-7">
-                        <p class="text-2xl font-semibold text-gray-900">58.16%</p>
-                        <p class="flex items-baseline ml-2 text-sm font-semibold text-green-600">
-                        <svg class="self-center flex-shrink-0 w-5 h-5 text-green-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z" clip-rule="evenodd" />
-                        </svg>
-                        <span class="sr-only"> Increased by </span>
-                        5.4%
+                        <p class="text-2xl font-semibold text-gray-900">
+                            58.16%
                         </p>
-                        <div class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6">
-                        <div class="text-sm">
-                            <a href="#" class="font-medium text-emerald-600 hover:text-emerald-500"> View all<span class="sr-only"> Avg. Open Rate stats</span></a>
-                        </div>
+                        <p
+                            class="flex items-baseline ml-2 text-sm font-semibold text-green-600"
+                        >
+                            <svg
+                                class="self-center flex-shrink-0 w-5 h-5 text-green-500"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span class="sr-only"> Increased by </span>
+                            5.4%
+                        </p>
+                        <div
+                            class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6"
+                        >
+                            <div class="text-sm">
+                                <a
+                                    href="#"
+                                    class="font-medium text-emerald-600 hover:text-emerald-500"
+                                >
+                                    View all<span class="sr-only">
+                                        Avg. Open Rate stats</span
+                                    ></a
+                                >
+                            </div>
                         </div>
                     </dd>
                 </div>
 
-                <div class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6">
+                <div
+                    class="relative px-4 pt-5 pb-12 overflow-hidden bg-white sm:px-6 sm:pt-6"
+                >
                     <dt>
                         <div class="absolute p-3 rounded-md bg-emerald-500">
-                        <svg class="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
-                        </svg>
+                            <svg
+                                class="w-6 h-6 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
+                                />
+                            </svg>
                         </div>
-                        <p class="ml-16 text-sm font-medium text-gray-500 truncate">Avg. Click Rate</p>
+                        <p
+                            class="ml-16 text-sm font-medium text-gray-500 truncate"
+                        >
+                            Avg. Click Rate
+                        </p>
                     </dt>
                     <dd class="flex items-baseline pb-6 ml-16 sm:pb-7">
-                        <p class="text-2xl font-semibold text-gray-900">24.57%</p>
-                        <p class="flex items-baseline ml-2 text-sm font-semibold text-red-600">
-                        <svg class="self-center flex-shrink-0 w-5 h-5 text-red-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z" clip-rule="evenodd" />
-                        </svg>
-                        <span class="sr-only"> Decreased by </span>
-                        3.2%
+                        <p class="text-2xl font-semibold text-gray-900">
+                            24.57%
                         </p>
-                        <div class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6">
-                        <div class="text-sm">
-                            <a href="#" class="font-medium text-emerald-600 hover:text-emerald-500"> View all<span class="sr-only"> Avg. Click Rate stats</span></a>
-                        </div>
+                        <p
+                            class="flex items-baseline ml-2 text-sm font-semibold text-red-600"
+                        >
+                            <svg
+                                class="self-center flex-shrink-0 w-5 h-5 text-red-500"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            <span class="sr-only"> Decreased by </span>
+                            3.2%
+                        </p>
+                        <div
+                            class="absolute inset-x-0 bottom-0 px-4 py-4 bg-gray-50 sm:px-6"
+                        >
+                            <div class="text-sm">
+                                <a
+                                    href="#"
+                                    class="font-medium text-emerald-600 hover:text-emerald-500"
+                                >
+                                    View all<span class="sr-only">
+                                        Avg. Click Rate stats</span
+                                    ></a
+                                >
+                            </div>
                         </div>
                     </dd>
                 </div>
