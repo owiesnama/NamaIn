8dc317b903ed491c49ef76967c53bd12ff904e96
khatabwedaa <khatabwedaa@gmail.com>
2023-04-19 17:05:19 +0200

    WIP

--- resources/js/Pages/Cheques/Index.vue
+++ resources/js/Pages/Cheques/Index.vue
@@ -7,6 +7,8 @@
     import { watch, reactive } from "vue";
     import { debounce } from "lodash";
     import EmptySearch from "@/Shared/EmptySearch.vue";
+    import Dropdown from "@/Components/Dropdown.vue";
+    import DropdownLink from "@/Components/DropdownLink.vue";
 
     defineProps({
         cheques: Object,
@@ -127,6 +129,23 @@
                             Debit
                         </button>
                     </div>
+
+                    <Dropdown align="right" width="48">
+                        <template #trigger>
+                            <button type="button" class="inline-flex items-center justify-center w-full px-3 py-2 mt-4 text-sm font-medium leading-4 text-gray-500 transition bg-white border border-gray-200 rounded-lg sm:w-auto sm:mt-0 gap-x-2 focus:border-emerald-300 focus:ring focus:ring-emerald-200 focus:ring-opacity-50 focus:outline-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                                </svg>
+
+                                Status
+                            </button>
+                        </template>
+
+                        <template #content>
+                            <button v-for="(key, sta) in status" :key="key" v-text="sta" class="block w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+                            </button>
+                        </template>
+                    </Dropdown>
                 </div>
             </div>
 
