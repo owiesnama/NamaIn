1681205e4fa199e345ede9bc36a9e29c5faa7a4c
Owiesnama <owiesnama@gmail.com>
2023-06-25 15:17:18 +0300

    defalut status for cheques

--- app/Enums/ChequeStatus.php
+++ app/Enums/ChequeStatus.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum ChequeStatus: string
 {
     case Drafted = 'drafted';
+    case Desirved = 'desirved';
     case Returned = 'returned';
     case Paid = 'paid';
     case PartialyPaid = 'partialy_paid';
@@ -13,6 +14,7 @@ enum ChequeStatus: string
     {
         return [
             'Drafted' => static::Drafted,
+            'Desirved' => static::Desirved,
             'Paid' => static::Paid,
             'Partialy Paid' => static::PartialyPaid,
             'Returned' => static::Returned,
--- resources/js/Shared/Cheque.vue
+++ resources/js/Shared/Cheque.vue
@@ -14,40 +14,58 @@
         },
     });
 
-    let isBeyondDue = computed(() => {
+    const isBeyondDue = computed(() => {
         return new Date(props.cheque.due) <= new Date();
     });
 
-    let form = useForm({
+    const form = useForm({
         status: props.cheque.status,
     });
 
-    let submit = () => {
+    const statusLable = (status) =>
+        Object.keys(props.chequeStatus).find(
+            (key) => props.chequeStatus[key] === status
+        );
+
+    const submit = () => {
         form.put(route("cheques.updateStatus", props.cheque.id));
     };
 </script>
 <template>
-    <div :class="cheque.is_credit  ? 'border-emerald-500' : 'border-red-500'" class="p-6 bg-white border-l-4 border-dashed rounded-lg rounded-l-none shadow-md rtl:border-l-0 rtl:border-r-4 rtl:rounded-l-lg rtl:rounded-r-none shadow-gray-200">
+    <div
+        :class="cheque.is_credit ? 'border-emerald-500' : 'border-red-500'"
+        class="p-6 bg-white border-l-4 border-dashed rounded-lg rounded-l-none shadow-md rtl:border-l-0 rtl:border-r-4 rtl:rounded-l-lg rtl:rounded-r-none shadow-gray-200"
+    >
         <div class="flex items-center justify-between">
-            <InputLabel
-                :value="'#' + cheque.id"
-            />
+            <InputLabel :value="'#' + cheque.id" />
 
-            <p class="text-sm" :class="isBeyondDue ? 'text-red-500 font-semibold' : 'text-gray-500 font-medium'" v-text="cheque.due_for_humans"></p>
+            <p
+                class="text-sm"
+                :class="
+                    isBeyondDue
+                        ? 'text-red-500 font-semibold'
+                        : 'text-gray-500 font-medium'
+                "
+                v-text="cheque.due_for_humans"
+            ></p>
         </div>
 
-        <h2 class="mt-1 text-lg font-semibold text-gray-800" v-text="cheque.payee.name + '  ' + cheque.status"></h2>
+        <h2
+            class="mt-1 text-lg font-semibold text-gray-800"
+            v-text="cheque.payee.name"
+        ></h2>
+        <span
+            class="text-sm text-gray-500 font-semibold"
+            v-text="__(statusLable(cheque.status))"
+        ></span>
 
-        
-        <div class="mt-24 sm:flex sm:items-end sm:justify-between" >
+        <div class="mt-24 sm:flex sm:items-end sm:justify-between">
             <div>
-                <InputLabel
-                   :value="__('Status')"
-                />
+                <InputLabel :value="__('Status')" />
                 <SelectBox
+                    v-model="form.status"
                     class="w-full mt-1 text-sm rounded-lg sm:w-36"
                     @change="submit"
-                    v-model="form.status"
                 >
                     <option
                         v-for="(key, status) in chequeStatus"
@@ -57,8 +75,12 @@
                     ></option>
                 </SelectBox>
             </div>
-            
-            <h3 :class="cheque.is_credit  ? 'text-emerald-500' : 'text-red-500'" class="mt-4 font-bold sm:mt-0" v-text="cheque.amount_formated"></h3>
+
+            <h3
+                :class="cheque.is_credit ? 'text-emerald-500' : 'text-red-500'"
+                class="mt-4 font-bold sm:mt-0"
+                v-text="cheque.amount_formated"
+            ></h3>
         </div>
     </div>
 </template>
