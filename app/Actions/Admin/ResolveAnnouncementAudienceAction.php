7d10093344caf5fa95588a6cf5afa58ac3026d71
owiesnama <37296520+owiesnama@users.noreply.github.com>
2026-07-04 16:08:28 +0300

    wip: scaffold notification system classes

---
+++ app/Actions/Admin/ResolveAnnouncementAudienceAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\Admin;
+
+class ResolveAnnouncementAudienceAction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
